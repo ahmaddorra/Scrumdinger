9fe1e2197b314c6445f03292d616aec9b62f3c94
ahmaddorra <ahmadeldorra1@gmail.com>
2021-03-08 02:13:37 +0200

    Initial commit

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,83 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Ahmad Dorra on 1/28/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Binding var scrum: DailyScrum
+    @State private var isPresented = false
+    @State private var data: DailyScrum.Data = DailyScrum.Data()
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView(scrum: $scrum)) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                        .accessibilityLabel(Text("Start meeting"))
+                }
+                HStack {
+                    Label("Length", systemImage: "clock").accessibilityLabel(Text("Meeting Length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color",systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill").foregroundColor(scrum.color)
+                }.accessibilityElement(children: .ignore)
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+            Section(header: Text("History")) {
+                if scrum.history.isEmpty {
+                    Label("No meetings yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history){ history in
+                    NavigationLink(destination: HistoryView(history: history)) {
+                        HStack{
+                            Image(systemName: "calendar")
+                            Text(history.date, style: .date)
+                        }
+                    }
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(Text(scrum.title))
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+            data = scrum.data
+        })
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView(scrumData: $data)
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    }, trailing: Button("Done") {
+                        isPresented = false
+                        scrum.update(from: data)
+                    } )
+            }
+        }
+        
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            DetailView(scrum: .constant(DailyScrum.data[0]))
+        }
+    }
+}
---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,24 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Ahmad Dorra on 2/11/21.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [String], lengthInMinutes: Int, transcript: String? = nil) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
+    }
+}
